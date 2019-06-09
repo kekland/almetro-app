cf06e2a789293404056f6d71316b3d59fca072b8
kekland <kk.erzhan@gmail.com>
2019-06-09 17:25:34 +0600

    impl: better bottom panel

--- lib/widgets/content_page/bottom_panel_new.dart
+++ lib/widgets/content_page/bottom_panel_new.dart
@@ -3,40 +3,50 @@ import 'package:almaty_metro/design/almetro_design.dart';
 import 'package:flutter/material.dart';
 
 class BottomPanelNew extends StatefulWidget {
+  
   @override
   _BottomPanelNewState createState() => _BottomPanelNewState();
 }
 
 class _BottomPanelNewState extends State<BottomPanelNew> {
-  int _departureStationIndex = 0;
-  int _arrivalStationIndex = MetroData.stations.length - 1;
+  int _stationIndex = 0;
   @override
   Widget build(BuildContext context) {
     return CardWidget(
       fluid: true,
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Откуда',
-            style: AlmetroTextStyle.caption,
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.chevron_left),
+            color: Colors.red,
+            onPressed: (_stationIndex != 0)
+                ? () => setState(() => _stationIndex--)
+                : null,
           ),
-          Text(
-            'Москва',
-            style: AlmetroTextStyle.boldInformation,
+          SizedBox(width: 16.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Станция',
+                  style: AlmetroTextStyle.caption,
+                ),
+                Text(
+                  MetroData.stations[_stationIndex],
+                  style: AlmetroTextStyle.boldInformation,
+                ),
+              ],
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: DividerWidget(),
-          ),
-          Text(
-            'Куда',
-            style: AlmetroTextStyle.caption,
-          ),
-          Text(
-            'Райымбек батыра',
-            style: AlmetroTextStyle.boldInformation,
+          SizedBox(width: 16.0),
+          IconButton(
+            icon: Icon(Icons.chevron_right),
+            color: Colors.red,
+            onPressed: (_stationIndex != MetroData.stations.length - 1)
+                ? () => setState(() => _stationIndex++)
+                : null,
           ),
         ],
       ),
